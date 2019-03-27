20521ca465d6e37c2768cb96da7e4a82bcf6b9de
Damien Ruckebusch <ruckebusch.damien@gmail.com>
2019-03-27 15:13:16 +0100

    adding Challenge 3 elements

--- src/App.js
+++ src/App.js
@@ -2,25 +2,51 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Quote from "./Quote";
+import Quotes from "./Quotes";
+import Lamp from "./Lamp";
+
 class App extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      working: false
+    };
+    };
+  handleClick = () => {
+    this.setState({ working: !this.state.working });
+  }
+    render(){
+      const working = this.state.working ? 'true' : 'false';
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={logo} 
+          className="App-logo"
+          className={working}
+          alt="logo" />
+          <h1 className="App-title">Simpsons Quotes</h1>
         </header>
+        <button 
+            onClick={this.handleClick}
+            
+        >Animation
+        </button>
+        <Lamp on/>
+        <Lamp />
+        <Quote
+          quote="I believe the children are the future... Unless we stop them now!"
+          character="Homer Simpson"
+          image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
+        />
+        <Quote
+          quote="Me fail English? That's unpossible"
+          character="Ralph Wiggum"
+          image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523"
+        />
+        <Quotes/>
       </div>
+
     );
   }
 }
---
+++ src/Quote.js
@@ -0,0 +1,14 @@
+// src/Quote.js
+import React from "react";
+
+const Quote = ({ quote, character, image }) => (
+  <figure>
+    <img src={image} alt={character} />
+    <figcaption>
+      <blockquote>{quote}</blockquote>
+      <cite>{character}</cite>
+    </figcaption>
+  </figure>
+);
+
+export default Quote;
